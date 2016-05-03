bf2fdace1381c19288988476375268ba819b3adc
charlie <chuckiels2011@gmail.com>
2016-05-03 19:52:54 -0400

    moved config values to config.scm

--- circ.scm
+++ circ.scm
@@ -1,13 +1,6 @@
 (require-extension irc posix posix-extras ncurses srfi-13)
 
-(define server "irc.nixers.net")
-(define channel "#nixers") ; TODO: allow array
-(define nick "nc")
-(define pfmt " > ")
-(define sep " | ")
-(define sep_col COLOR_CYAN)
-(define nick_col COLOR_BLUE)
-(define nick_len 10)
+(load "config.scm")
 
 (define con (irc:connection server: server nick: nick))
 
---
+++ config.scm
@@ -0,0 +1,8 @@
+(define server "irc.nixers.net")
+(define channel "#nixers") ; TODO: allow array
+(define nick "nc")
+(define pfmt " > ")
+(define sep " | ")
+(define sep_col COLOR_CYAN)
+(define nick_col COLOR_BLUE)
+(define nick_len 10)
